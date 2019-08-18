c67b56acbe4fcd71669f18fe7d93315b00366755
Donghoon Han <dhk1349@naver.com>
2019-08-18 15:55:30 +0900

    chat_bot module
    waiting for "Kakao i openbuilder" reply for service

--- KHU_crawler/crwaler_module.py
+++ KHU_crawler/crwaler_module.py
@@ -9,49 +9,62 @@ from bs4 import BeautifulSoup as soup
 import requests
 import datetime
 
-def SWedu():
+def SWedu(today1):
     url="http://swedu.khu.ac.kr/board5/bbs/board.php?bo_table=06_01"
     html=requests.get(url).text
     obj=soup(html, "html.parser")
 
     table=obj.find("div", {"class":"colgroup"}).find("div",{"id":"bo_list"}).find("form",{"id":"fboardlist"}).find("tbody").findAll("tr")
-
+    newsbox=[]
     for i in table:
-        title=i.find("td", {"class":"td_subject"})
         date=i.find("td",{"class":"td_datetime"}).text.strip()
-        title=title.find("div",{"class":"bo_tit"})
-        title=title.find("a").text.strip() #제목
-        print(title,"  ",date)
+        if(date==today):
+            title=i.find("td", {"class":"td_subject"})
+            title=title.find("div",{"class":"bo_tit"})
+            title=title.find("a").text.strip() #제목
+            newsbox.append(title)
+    if(len(newsbox)==0):
+        newsbox.append("No New Issue on SW중심사업단\n")
+    return newsbox
 
-
-def KIC():
+def KIC(today2):
     url="http://kic.khu.ac.kr/notice/undergraduate/"
     html=requests.get(url).text
     obj=soup(html, "html.parser")
 
     table=obj.find("body").find("article").find("div", {"class":"kboard-list"}).findAll("tr")
     table=table[1:]
+    newsbox=[]
     for row in table:
-        title=row.find("td",{"class":"kboard-list-title"}).find("a").find("div",{"class":"kboard-default-cut-strings"}).text.strip()
-
         date=row.find("td",{"class":"kboard-list-date"}).text.strip()
-        print(title,"   ",date)    
-
-def SoftwareKHU():
+        if(date==today):
+            title=row.find("td",{"class":"kboard-list-title"}).find("a").find("div",{"class":"kboard-default-cut-strings"}).text.strip()
+            newsbox.append(title)
+    if(len(newsbox)==0):
+        newsbox.append("No NEW Issue on KIC\n")
+    return newsbox
+        
+def SoftwareKHU(today1):
     url="http://software.khu.ac.kr/board5/bbs/board.php?bo_table=05_01"
     html=requests.get(url).text
     obj=soup(html, "html.parser")
 
     table=obj.find("div",{"class":"sContainer"}).find("tbody").findAll("tr")
-
+    newsbox=[]
     for row in table:
-        title=row.find("div",{"class":"bo_tit"}).text.strip()    
         date=row.find("td",{"class":"td_datetime"}).text.strip()
-        print(title,"   ",date)  
-
-SWedu() # 사업단 공지사
-KIC() #국제대학 공지사항
-SoftwareKHU()#소프트웨어 중심대학 공지사항
-
+        if(date==today):
+            title=row.find("div",{"class":"bo_tit"}).text.strip()    
+            newsbox.append(title)
+    if(len(newsbox)==0):
+        newsbox.append("No New Issue on SW융합대학\n")
+    return newsbox
+    
 dt=datetime.datetime.now()
-print(dt.year," ",dt.month," ",dt.day)
+today=str(dt)
+today1=today[0:10]
+today2=today[0:10].replace("-",".")
+print(SWedu(today1)) # 사업단 공지사
+print(KIC(today)) #국제대학 공지사항
+print(SoftwareKHU(today))#소프트웨어 중심대학 공지사항
+
---
+++ chat_bot_module/chat_bot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 15:17:05 2019
+
+@author: donghoon
+"""
+import os
+from flask import Flask, request, jsonify
+import json
+
+app=Flask(__name__)
+
+default_buttons1=['button1', 'button2']
+
+default_buttons2=["welcome","to","Kakao"]
+
+@app.route('/keyboard')
+def keyboard():
+    return jsonify({'type' : 'buttons', 'buttons':default_buttons1})
+
+@app.route('/message', methods=["post"])
+    def button1():
+        response=request.get_json()
+        user_reponse=response["content"]
+        
+        if(user_reponse in default_buttons1):
+            bot_res={
+                    'message':{"text":"Hello this First layer"},
+                    "keyboard":{"buttons":default_buttons2,
+                                "type":"buttons",},
+                    }
+        else if(user_response==):
+            bot_res={
+                    "message":{"text":"Hello this is seconde layer"},
+                    "keyboard":{"buttons":default_buttons1, 
+                                "type":"buttons",},
+                    }
+        return jsonify(bot_res)
+
+if __name__="__main__":
+    app.run(host="0.0.0.0", port=5000)
