779a352fc55075c73ad70ef8430b70b261e49a4c
dhk1349 <dhk1349@naver.com>
2019-10-24 01:44:14 +0000

    .

--- server_test.py
+++ server_test.py
@@ -14,9 +14,8 @@ app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    print("Welcome to my page")
-    return
+    return ("welcome")
 
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
